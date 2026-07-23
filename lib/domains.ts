7262c2e0b9e88c6306ef3c556bb6457f8caee367
2xBuild <ha29rp@gmail.com>
2026-07-23 10:46:08 +0530

    .

--- lib/domains.ts
+++ lib/domains.ts
@@ -2,6 +2,15 @@ export type Domain = { name: string; href: string; logo?: string };
 
 export const domains: Domain[] = [
   { name: "drawi.ai", href: "https://drawi.ai", logo: "https://drawi.ai/favicon.ico" },
+    { name: "kollab.to", href: "https://drawi.ai", logo: "https://kollab.to/favicon.ico" },
+      { name: "wiral.app", href: "https://wiral.app", logo: "https://wiral.app/favicon.ico" },
+        { name: "nearmate.io", href: "https://nearmate.io", logo: "https://nearmate.io/favicon.ico" },
+          { name: "onepurplepen.com", href: "https://onepurplepen.com", logo: "https://onepurplepen.com/favicon.ico" },
+
+  { name: "dotschool.org", href: "https://dotschool.org", logo: "https://dotschool.org/icon.svg" },
+    { name: "cutefol.io", href: "https://cutefol.io", logo: "https://cutefol.io/icon.svg" },
+      { name: "void.ac", href: "https://void.ac", logo: "https://void.ac/icon.svg" },
+        
   { name: "buildzak.com", href: "https://buildzak.com", logo: "https://buildzak.com/favicon.ico" },
   { name: "oneDB.net", href: "https://oneDB.net", logo: "https://onedb.net/icon.svg" },
   { name: "a9ents.com", href: "https://a9ents.com", logo: "https://a9ents.com/favicon.ico" },
